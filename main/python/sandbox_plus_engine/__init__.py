3fed8ce34cb107d6a5099d57ca536a85035366b7
lzhbhlrPython <Liubaolin20070125@outlook.com>
2020-10-30 17:41:18 +0800

    change_cipher

--- main/python/sandbox_plus_engine/__init__.py
+++ main/python/sandbox_plus_engine/__init__.py
@@ -1,2 +1,2 @@
-from .simpleCipher.displacement_cipher import encode as displacement_encode
-from .simpleCipher.displacement_cipher import decode as displacement_decode
+from .simple_cipher.displacement_cipher import encode as displacement_encode
+from .simple_cipher.displacement_cipher import decode as displacement_decode
